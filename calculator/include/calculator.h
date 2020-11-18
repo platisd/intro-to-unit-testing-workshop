db43a0196b6f368e891ac2d576f1ae6c5784511e
platisd <platisd@users.noreply.github.com>
2020-11-18 21:49:36 +0100

    Apply clang-format

--- calculator/include/calculator.h
+++ calculator/include/calculator.h
@@ -1,12 +1,13 @@
 #ifndef ARDUINO_UNIT_TEST_PROJECT_CALCULATOR_H
 #define ARDUINO_UNIT_TEST_PROJECT_CALCULATOR_H
 
-namespace calculator {
-    int add(int a, int b);
+namespace calculator
+{
+int add(int a, int b);
 
-    int subtract(int a, int b);
+int subtract(int a, int b);
 
-    double divide(int a, int b);
-}
+double divide(int a, int b);
+} // namespace calculator
 
-#endif //ARDUINO_UNIT_TEST_PROJECT_CALCULATOR_H
+#endif // ARDUINO_UNIT_TEST_PROJECT_CALCULATOR_H
